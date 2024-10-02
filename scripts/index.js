310714ff4b605884927bba538914d8b593a7ee78
emeksdot <emeksdot@yahoo.com>
2024-10-02 06:33:57 +0100

    I had to redo the css files due to some mistakes

--- scripts/index.js
+++ scripts/index.js
@@ -2,4 +2,18 @@ const dateObj = new Date();
 console.log(dateObj)
 
 let currentYear = document.querySelector("#currentyear");
-currentYear.textContent = dateObj.getFullYear()
+currentYear.textContent = dateObj.getFullYear();
+
+const hamburger = document.querySelector("#mode");
+const navLinks = document.querySelector(".navigation");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("show");
+    navLinks.classList.toggle("show");
+});
+
+
+let lastModif = new Date(document.lastModified);
+const modified = document.querySelector("#lastModified");
+modified.innerHTML = lastModif;
+
