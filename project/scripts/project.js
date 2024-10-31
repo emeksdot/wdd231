b6a7c0e99e0233042183b90599ccd5992cbdca61
emeksdot <emeksdot@yahoo.com>
2024-10-31 16:35:03 +0100

    tested the responsiveness of the home page- its okay

---
+++ project/scripts/project.js
@@ -0,0 +1,21 @@
+
+
+
+
+// Create array of Product objects
+
+const products = [
+    {
+        title: "https://",
+        brand: "https://",
+        imgURL: "https://",
+        price: 225,
+        desc: "string",
+        
+    },
+]
+
+
+// Create the Product cards
+
+const card = document.createElement('section');
