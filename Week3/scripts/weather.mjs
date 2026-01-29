25ee89ea5d0a7e06230e3a6131983ff53e9d25b3
emeksdot <emeksdot@yahoo.com>
2026-01-29 19:20:27 +0100

    completed the openweathermap task

---
+++ Week3/scripts/weather.mjs
@@ -0,0 +1,33 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.63&appid=4d4a0b19d23373e66431d7167db6a8aa&units=imperial";
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const displayResults = (data) => {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconlink = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  let weatherDescription = data.weather[0].description;
+  weatherIcon.setAttribute("src", iconlink);
+  weatherIcon.setAttribute("alt", weatherDescription);
+  captionDesc.textContent = `${weatherDescription}`;
+};
+
+apiFetch();
